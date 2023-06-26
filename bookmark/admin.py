d7f4e37cd95a8d8b713139209eea1b3fcc42b95c
성열 <csy5501@gmail.com>
2023-06-26 11:15:00 +0900

    0626 2 update admin model

--- bookmark/admin.py
+++ bookmark/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from bookmark.models import Bookmark
 
 # Register your models here.
+
+# not using decorator
+# admin.site.register(Bookmark, BookmarkAdmin):
+@admin.register(Bookmark)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'url')
