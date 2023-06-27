5a655ea68a4d00fb2eeff4fa48f0affb6cd5a8d0
성열 <csy5501@gmail.com>
2023-06-27 15:18:47 +0900

    0627 1 added taggit_post_list.html

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView, ArchiveIndexView, YearArchiveView, MonthArchiveView, \
     DayArchiveView, TodayArchiveView, TemplateView
 from blog.models import Post
+
 # Create your views here.
 class PostLV(ListView):
     model = Post
