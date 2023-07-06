39bd66f50a82d48a2c665a7d6e5342da2d79116e
성열 <csy5501@gmail.com>
2023-07-06 09:25:23 +0900

    0706 update photo/models.py & migrated

--- photo/models.py
+++ photo/models.py
@@ -7,7 +7,8 @@ from photo.fields import ThumbnailImageField
 # Create your models here.
 class Album(models.Model):
     name = models.CharField(max_length=30)
-    description  = models.CharField('One Line Description', max_length=100, blank=True)
+    description = models.CharField('One Line Description', max_length=100, blank=True)
+    owner = models.ForeignKey('auth.User', on_delete=models.CASCADE, verbose_name='OWNER', blank=True, null=True)
 
     class Meta:
         ordering = ('name',)
@@ -25,6 +26,7 @@ class Photo(models.Model):
     description = models.TextField('Photo Description', blank=True)
     image = ThumbnailImageField(upload_to='photo/%Y/%m')
     upload_dt = models.DateTimeField('Upload Date', auto_now_add=True)
+    owner = models.ForeignKey('auth.User', on_delete=models.CASCADE, verbose_name='OWNER', blank=True, null=True)
 
     class Meta:
         ordering = ('title',)
@@ -34,4 +36,3 @@ class Photo(models.Model):
 
     def get_absolute_url(self):
         return reverse('photo:photo_detail', args=(self.id,))
-
